727507c151e3d8a25023532b04a6d7ef98d2a61c
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-05-22 02:13:24 +0800

    init seeder

--- database/factories/DiscussionPostFactory.php
+++ database/factories/DiscussionPostFactory.php
@@ -17,6 +17,12 @@ class DiscussionPostFactory extends Factory
             'user_id' => $this->faker->numberBetween(6, 19),
             'title' => $this->faker->realText(50),
             'body' => $this->faker->realText(300),
+            'is_hobby' => false,
+            'is_wellbeing' => false,
+            'is_career' => false,
+            'is_coaching' => false,
+            'is_science_and_tech' => false,
+            'is_social_cause' => false,
             'slug' => $this->faker->uuid(),
             'created_at' => $this->faker->dateTimeBetween('-2 years', 'now'),
         ];
