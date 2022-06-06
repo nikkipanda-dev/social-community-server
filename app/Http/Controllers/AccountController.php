f5366155bfe2a4246a026c1728594c6c1d8e5d56
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-06-06 16:38:12 +0800

    fixes auth and user store response

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -229,13 +229,6 @@ class AccountController extends Controller
                         'token' => $userResponse['token'],
                         'user' => $userResponse['user'],
                         'firebase' => [
-                            'api_key' => env("FIREBASE_API_KEY"),
-                            'auth_domain' => env("FIREBASE_AUTH_DOMAIN"),
-                            'database_url' => env("FIREBASE_DATABASE_URL"),
-                            'project_id' => env("FIREBASE_PROJECT_ID"),
-                            'storage_bucket' => env("FIREBASE_STORAGE_BUCKET"),
-                            'messaging_sender_id' => env("FIREBASE_MESSAGING_SENDER_ID"),
-                            'app_id' => env("FIREBASE_APP_ID"),
                             'secret' => $credential->secret,
                         ]
                     ]);
--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -48,13 +48,6 @@ class AuthController extends Controller
                             'user' => $user,
                             'token' => $token,
                             'firebase' => [
-                                'api_key' => env("FIREBASE_API_KEY"),
-                                'auth_domain' => env("FIREBASE_AUTH_DOMAIN"),
-                                'database_url' => env("FIREBASE_DATABASE_URL"),
-                                'project_id' => env("FIREBASE_PROJECT_ID"),
-                                'storage_bucket' => env("FIREBASE_STORAGE_BUCKET"),
-                                'messaging_sender_id' => env("FIREBASE_MESSAGING_SENDER_ID"),
-                                'app_id' => env("FIREBASE_APP_ID"),
                                 'secret' => $credential->secret,
                             ]
                         ]);
